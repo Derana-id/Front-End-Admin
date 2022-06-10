5710cb2e486923b5219742c4fa0a8f6ded50c25f
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-06-10 18:30:43 +0700

    basic layout

---
+++ components/atoms/control-sidebar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function index() {
+  return (
+    <aside className="control-sidebar control-sidebar-dark">
+      {/* Control sidebar content goes here */}
+      <div className="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
+      </div>
+    </aside>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+/* eslint-disable no-unused-vars */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
